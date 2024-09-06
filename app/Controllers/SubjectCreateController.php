48e5a13dac19e362fab106897adbbb7df2167792
VeraDosSantos <vera.dos-santos@lepoles.org>
2024-09-06 16:57:37 +0200

    creation d'un sujet

--- app/Controllers/SubjectCreateController.php
+++ app/Controllers/SubjectCreateController.php
@@ -3,6 +3,26 @@
 $role = $_SESSION['user']['role'];
 
 if ($role === 'admin') {
+    if (isset($_POST['name'])) {
+
+       $name = $_POST['name'];
+       $description = $_POST['description'];
+        $creationDate = date("Y-m-d");
+
+        $subjectQuery = "INSERT INTO subject (name, description, creation_date) VALUES (:name, :description, :creation_date)";
+        // je prepare ma requete sql a l'envoie
+        $subjectStatement = $mysqlClient->prepare($subjectQuery);
+        //  je modifie les valeurs de ma requete en fonction des valeurs du formulaire
+        $subjectStatement->bindParam(':name', $name);
+        $subjectStatement->bindParam(':description', $description);
+        $subjectStatement->bindParam(':creation_date', $creationDate);
+
+        // j'execute la requete
+        $subjectStatement->execute();
+
+        // je redirige l'utilisateur vers la page user pour qu'il vois le nouveau produit
+        redirectToRoute('/');
+    }
     require_once(__DIR__ . '/../Views/subject/subject.create.view.php');
 } else {
     redirectToRoute('/');
--- app/Views/subject/subject.create.view.php
+++ app/Views/subject/subject.create.view.php
@@ -3,7 +3,22 @@ $title = "Création d'un sujet";
 require_once(__DIR__ . "/../partials/head.php");
 ?>
 <h1>Tes admin tu peux créer un nouveau sujet !</h1>
-
+<form method="POST">
+    <div class="col-md-4 mx-auto d-block mt-5">
+        <div class="mb-3">
+            <label for="name" class="form-label">Nom</label>
+            <input type="text" class="form-control" name='name'>
+        </div>
+        <div class="mb-1">
+            <label for="description" class="form-label">Description</label>
+            <textarea class="form-control" name="description"></textarea>
+        </div>
+        <button type="submit" class="btn colorPurpel mt-3">Création du sujet</button>
+    </div>
+</form>
+<?php if (isset($error)) {
+    echo "<p class='text-danger'>" . $error . "<p>";
+} ?>
 
 <?php
 require_once(__DIR__ . "/../partials/footer.php");
