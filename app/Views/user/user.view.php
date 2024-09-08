e2b3387244ad0493d3f9f9d2e6ee4dffcb75f5e6
nanaplouf <vera78ds@gmail.com>
2024-09-08 20:03:21 +0200

    %µ%µ%Âise à jour du

---
+++ app/Views/user/user.view.php
@@ -0,0 +1,11 @@
+<?php
+global $subject;
+$title = "Profil";
+require_once(__DIR__ . "/partials/head.php");
+?>
+
+    <h1>Bienvenue à toi</h1>
+
+<?php
+require_once(__DIR__ . "/partials/footer.php");
+?>
--- config/router.php
+++ config/router.php
@@ -2,8 +2,13 @@
 require_once(__DIR__ . "/function.php");
 require_once(__DIR__ . '/db.php');
 
+// Récupération de l'URI actuelle de la requête utilisateur
+// Cette partie extrait uniquement le chemin de l'URL (sans les paramètres GET ou les fragments)
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
+// Définition des routes
+// Ce tableau associe des chemins d'URI à des fichiers de contrôleurs spécifiques
+// Le chemin dans l'URL (comme '/login') est relié au contrôleur correspondant (comme 'LoginController.php')
 $routes = [
     '/' => 'HomeController.php',
     //la connection
@@ -20,9 +25,15 @@ $routes = [
     '/article-edit' => 'ArticleEditController.php', //modifier un article
 ];
 
+// Vérification de l'existence de la route dans le tableau des routes
+// Si l'URI demandée existe dans le tableau, le contrôleur associé est inclus
 if (array_key_exists($uri, $routes)) {
+    // Inclusion dynamique du fichier contrôleur correspondant à l'URI
     require_once(__DIR__ . "/../app/Controllers/" . $routes[$uri]);
 } else {
+    // Si l'URI n'existe pas dans le tableau des routes, renvoie une erreur 404
+    // http_response_code(404) indique que la page n'a pas été trouvée
     http_response_code(404);
+    // Inclusion du fichier 404.php pour gérer l'affichage d'une page d'erreur personnalisée
     require_once(__DIR__ . '/../app/Controllers/404.php');
 }
